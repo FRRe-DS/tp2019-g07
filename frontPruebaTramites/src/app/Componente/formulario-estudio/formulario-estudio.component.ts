d5fcad264e855512ff51a31747b7735fcf91a3d2
gonzaFac <gonzaloperessi@gmail.com>
2019-06-28 14:11:59 -0300

    seguimos con formulario de entrada de datos

--- frontPruebaTramites/src/app/Componente/formulario-estudio/formulario-estudio.component.ts
+++ frontPruebaTramites/src/app/Componente/formulario-estudio/formulario-estudio.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Estudio } from 'src/app/Modelo/estudio';
+import { CategoriaExamen } from 'src/app/Modelo/categoriaexamen';
+import { Tramite } from 'src/app/Modelo/tramite';
+import {EstudioService} from  'src/app/Servicio/estudio.service';
 @Component({
   selector: 'app-formulario-estudio',
   templateUrl: './formulario-estudio.component.html',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormularioEstudioComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  miCategoriaExamen: CategoriaExamen = {
+    codDato: 20,
+    descripcionDato: '',
+  };
+
+
+  miEstudio: Estudio = {
+    ideTramiteExamenDatoLinea: 0,
+    tramite: null,
+    categoriaExamen: this.miCategoriaExamen,
+    resultadoExamen: '',
+    estadoEstudio: '',
+    fechaEstudio: new Date(),
+  };
+
+
+constructor( private estudioService: EstudioService ) { }
+
+ngOnInit() {
   }
+
+  saveNewEstudio
 
 }
